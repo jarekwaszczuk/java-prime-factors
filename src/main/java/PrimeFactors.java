f1072f9dd3861a63dd5dd643c8fba9ad2bd1fa25
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-05-25 00:21:20 +0200

    14 testów

---
+++ src/main/java/PrimeFactors.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrimeFactors {
+
+    public PrimeFactors() {
+    }
+
+    public static List<Integer> generate(int i) {
+        List<Integer> lista = new LinkedList();
+
+        if (i == 1) {
+            lista.add(1);
+        } else {
+            //TODO
+        }
+
+        return lista;
+    }
+}
--- src/test/java/PrimeFactorsTests.java
+++ src/test/java/PrimeFactorsTests.java
@@ -1,9 +1,134 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class PrimeFactorsTests {
+    private List lista;
 
     @Test
-    public void generate1(){
+    public void generate1() {
+        PrimeFactors prime = new PrimeFactors();
 
+        lista = prime.generate(1);
+
+        Assertions.assertTrue(lista.size() == 1);
+    }
+
+    @Test
+    public void generate2() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(2);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate3() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(3);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate4() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(4);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate5() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(5);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate6() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(6);
+
+        Assertions.assertTrue(lista.size() == 3);
+    }
+
+    @Test
+    public void generate7() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(7);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate8() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(8);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate9() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(9);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate10() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(10);
+
+        Assertions.assertTrue(lista.size() == 3);
+    }
+
+    @Test
+    public void generate11() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(11);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate12() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(12);
+
+        Assertions.assertTrue(lista.size() == 3);
+    }
+
+    @Test
+    public void generate13() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(13);
+
+        Assertions.assertTrue(lista.size() == 2);
+    }
+
+    @Test
+    public void generate14() {
+        PrimeFactors prime = new PrimeFactors();
+
+        lista = prime.generate(14);
+
+        Assertions.assertTrue(lista.size() == 3);
     }
 }
