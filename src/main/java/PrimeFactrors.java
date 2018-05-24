1c166eb79c9b0f382aca004b0a9044e3f360d0cd
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-05-25 00:02:21 +0200

    Szablon klasy i testów

---
+++ src/main/java/PrimeFactrors.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrimeFactrors {
+    public static List<Integer> generate(){
+        List<Integer>  lista = new LinkedList();
+
+
+        return lista;
+    }
+}
---
+++ src/test/java/PrimeFactorsTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class PrimeFactorsTests {
+
+    @Test
+    public void generate1(){
+
+    }
+}
